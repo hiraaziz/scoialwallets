312cf1f630d6031e168899f254a1929ab006d55a
hiraaziz <hiraaziz1998@gmail.com>
2023-04-14 01:15:48 +0500

    Update page.tsx

--- wagmi/app/rainbow/page.tsx
+++ wagmi/app/rainbow/page.tsx
@@ -16,8 +16,7 @@ import { publicProvider } from "wagmi/providers/public";
 import { createClient } from "wagmi";
 import { RainbowKitProvider, ConnectButton } from "@rainbow-me/rainbowkit";
 function RainbowKitExample() {
-  const projectId =
-    "BDnXx3nIoms1zqtd4z6B1ud4zCE261GoMcPb0LP4QI4lA2J1Q60esCWm_XZri-AVSFOHKY2cE620zU57aasRxYQ";
+  const projectId = "1cd7f081-0a48-4038-9f30-743067be10b8";
   const connectors = connectorsForWallets([
     {
       groupName: "Social",
